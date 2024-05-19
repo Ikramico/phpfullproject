395b01c26ca6b10b6306c5b81ac2a1eb94ac6844
Ikram Ico <ikramicono@gmail.com>
2024-05-19 10:01:14 +0600

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = '';
+$dbName = 'movie';
+
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Hi,
+        <?php
+        echo $_REQUEST['username'].' '. $_REQUEST['email']
+        ?>
+    </h2>
+    <?php
+    require_once('./config.php');
+
+    if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    // Execute a MySQL query
+    $sql = "SELECT * FROM movies";
+    $result = $mysqli->query($sql);
+
+    if (!$result) {
+    die("Error: " . $mysqli->error);
+    }
+
+    // Process the query results
+    while ($row = $result->fetch_assoc()) {
+    // Process each row of data
+    echo " Name: " . $row["Name"] ." Genre".$row["Genre"]."<img src= '" .$row["Image"]."'><br>";
+    }
+
+    // Close the database connection
+    $mysqli->close();
+    ?>
+
+</body>
+
+</html>
